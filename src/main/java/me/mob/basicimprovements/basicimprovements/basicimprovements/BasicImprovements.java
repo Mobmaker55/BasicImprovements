7db267f40ff34f1183c87df703a8c39f9f996a98
Mobmaker55 <45888585+Mobmaker55@users.noreply.github.com>
2020-09-02 14:26:18 -0400

    first commit
    Signed-off-by: Mobmaker55 <45888585+Mobmaker55@users.noreply.github.com>

---
+++ src/main/java/me/mob/basicimprovements/basicimprovements/basicimprovements/BasicImprovements.java
@@ -0,0 +1,17 @@
+package me.mob.basicimprovements.basicimprovements.basicimprovements;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BasicImprovements extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
